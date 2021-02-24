8b9ba0e14099d0431183a96b208598bbb9d8703d
Domenico Dongiovanni <domenico.dongiovanni@gmail.com>
2021-02-24 18:10:08 +0100

    added cards to findfriends

--- src/components/Feed/FindFriends/FindFriendsSearch/index.js
+++ src/components/Feed/FindFriends/FindFriendsSearch/index.js
@@ -1,13 +1,13 @@
+import { MainContainer, CardContainer, FriendsContainer, CardImg, ThingsUserLikesButton, FollowingButton, TransparentButton } from '../../../../style-feed/FindFriends/layout.js'
 import { connect } from 'react-redux';
 import React from 'react';
 
 class FindFriendsSearch extends React.Component {
     constructor(props){
-    super(props)
-    this.state = {
-     
+        super(props)
+        this.state = {
+                }
     }
-}
 
      fetch = (e) => {
         e.preventDefault();
@@ -22,7 +22,6 @@ class FindFriendsSearch extends React.Component {
             method: method,
             headers: headers,
         };
-
         fetch(url,config)
             .then(res => res.status ? res.json() : console.log('users response not ok'))
             .then(data => {
@@ -31,11 +30,28 @@ class FindFriendsSearch extends React.Component {
      }
 
     render (){
-        console.log(this.props)
+         console.log(this.props)
         return (
             <>
-            <h1>test</h1>
+               
             <button onClick={this.fetch}>find friends</button>
+            <MainContainer>
+                <FriendsContainer>
+                   
+                    {this.props.users.map((user, index) => 
+                             <CardContainer className={user.id}>  
+                             <CardImg src={user.avatar}/>
+                            <div><h1>{user.first_name} {user.last_name}</h1></div>
+                            <div><p>{user.location}</p> </div> 
+                            <div><p>{user.about_me}</p></div>
+                            <div> <FollowingButton value={user.logged_in_user_is_following?"Following": ""} />                      
+                            <TransparentButton /></div>
+                            <div>{user.things_user_likes.map((things)=><ThingsUserLikesButton>{things}</ThingsUserLikesButton>)}</div>
+                            </CardContainer>
+                     )}
+                   
+                </FriendsContainer>
+            </MainContainer>
 
             </>
         )
--- src/components/Feed/FindFriends/index.js
+++ src/components/Feed/FindFriends/index.js
@@ -2,6 +2,7 @@ import {NavBar}  from '../nav-bar';
 import SearchBar from '../SearchBar';
 import FindFriendsSearch from './FindFriendsSearch';
 
+
 const FindFriends = () => {
     
     return (
