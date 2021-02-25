07b838d5cdc1535182ff6c5ef13be6a04666feab
David Brown <davidbr996@gmail.com>
2021-02-25 14:26:10 +0100

    yay

--- src/components/Feed/Profile/ProfileBody/ProfilePosts/index.js
+++ src/components/Feed/Profile/ProfileBody/ProfilePosts/index.js
@@ -1,8 +1,36 @@
 import { ProfilePostsContainer } from '../../../../../style-feed/Profile/layout.js';
+import { useEffect } from 'react';
+
 
 const ProfilePosts = () => {
+    useEffect( () => {
+        const getPostsFunc = (newPost) => {
+            const token = localStorage.getItem('token');
+            const url = "https://motion.propulsion-home.ch/backend/api/social/posts/me/";
+            const method = 'GET';
+            const headers = new Headers({
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`
+            });
+            const config = {
+                method: method,
+                headers: headers
+            };
+            fetch(url, config)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+          }
+        getPostsFunc();
+    }, [])
+
+
+
     return (
-        <ProfilePostsContainer>hello</ProfilePostsContainer>
+        <ProfilePostsContainer>
+
+        </ProfilePostsContainer>
     )
 }
 
